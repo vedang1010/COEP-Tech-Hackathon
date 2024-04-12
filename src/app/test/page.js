41d388e43ca55508e3f7d6fe5c4b49b7f87b8142
Shriya Kale <kaleshriya064@gmail.com>
2024-04-12 17:07:50 +0530

    Woking on remark

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,18 +1,15 @@
 "use client"
 import React from 'react'
 import Layout from '../../../components/Layout';
-// import { useClient } from 'next-client';
 import Remarks from '../remark/remark';
 
 const page = () => {
-  // useClient();
   
   return (
-    <>
-    
+    <>   
         <Remarks />
     </>
   )
 }
 
-export default home
+export default page
