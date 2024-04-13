71a3ff334085d3ffbb8f9f9bb0fcde3bf07c0e9e
Chiro2002 <sanchitrajmane2002@gmail.com>
2024-04-13 08:16:22 +0530

    adding css to fac incharg dashboard

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,13 +45,7 @@ const auth=getAuth()
         
       <VenueListButton onClick={handleHomeChange}>Home</VenueListButton>
 
-
-      <VenueListButton onClick={handleDisplayIncharge}>Approve Locations</VenueListButton>
-      <VenueListButton onClick={handleDisplayFaculty}>Approve Requests</VenueListButton>
-      <VenueListButton onClick={handleDisplayChange}>Request</VenueListButton>
-
-
-      {/* {
+      {
          typeof window !== 'undefined' &&
         (position === "Venue In Charge" ? (
           <VenueListButton onClick={handleDisplayIncharge}>Approve Locations</VenueListButton>
@@ -60,7 +54,7 @@ const auth=getAuth()
         ) : (
           <VenueListButton onClick={handleDisplayChange}>Request</VenueListButton>
         ))
-      } */}
+      }
       <VenueListButton onClick={handlePageChange}>Venue</VenueListButton>
       <LogoutButton onClick={handleSignOut}>Log out</LogoutButton>
     </NavbarContainer>
